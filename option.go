3c9356e4abd36503ecae3483ededbeb3eb75f2ac
Andrea Oliveti <aoliveti@cloud4wi.com>
2024-03-02 21:47:26 +0100

    Add documentation for struct and methods. buildData now skips the body option if r.Body is http.NoBody.

--- option.go
+++ option.go
@@ -8,36 +8,44 @@ const (
 
 type Option func(curling *Command)
 
+// WithFollowRedirects enables the option -L, --location.
 func WithFollowRedirects() Option {
 	return func(curling *Command) {
 		curling.location = true
 	}
 }
 
+// WithCompression enables the option --compressed.
 func WithCompression() Option {
 	return func(curling *Command) {
 		curling.compressed = true
 	}
 }
 
+// WithInsecure enables the option [-k, --insecure]
 func WithInsecure() Option {
 	return func(curling *Command) {
 		curling.insecure = true
 	}
 }
 
+// WithLongForm enables the long form for cURL options.
+// Example: --header instead of -H.
 func WithLongForm() Option {
 	return func(curling *Command) {
 		curling.useLongForm = true
 	}
 }
 
+// WithSilent enables the option -s, --silent.
 func WithSilent() Option {
 	return func(curling *Command) {
 		curling.silent = true
 	}
 }
 
+// WithMultiLine splits the command across multiple lines.
+// The default line continuation character is backslash.
 func WithMultiLine() Option {
 	return func(curling *Command) {
 		curling.useMultiLine = true
@@ -45,6 +53,8 @@ func WithMultiLine() Option {
 	}
 }
 
+// WithWindowsMultiLine splits the command across multiple lines.
+// The line continuation character is caret.
 func WithWindowsMultiLine() Option {
 	return func(curling *Command) {
 		curling.useMultiLine = true
@@ -52,6 +62,8 @@ func WithWindowsMultiLine() Option {
 	}
 }
 
+// WithPowerShellMultiLine splits the command across multiple lines.
+// The line continuation character is backtick.
 func WithPowerShellMultiLine() Option {
 	return func(curling *Command) {
 		curling.useMultiLine = true
@@ -59,6 +71,7 @@ func WithPowerShellMultiLine() Option {
 	}
 }
 
+// WithDoubleQuotes enables escaping using double quotes.
 func WithDoubleQuotes() Option {
 	return func(curling *Command) {
 		curling.useDoubleQuotes = true
